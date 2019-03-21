715ee5e9c4558214cb1cf4d424fa93411e6486aa
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2019-03-21 07:10:36 +0700

    first commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.Color.SeaShell;
+            this.label1.Location = new System.Drawing.Point(203, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // email
+            // 
+            this.email.Font = new System.Drawing.Font("Helvetica", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.email.Location = new System.Drawing.Point(120, 125);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(314, 34);
+            this.email.TabIndex = 8;
+            this.email.Text = "username";
+            this.email.Click += new System.EventHandler(this.username_Click);
+            this.email.TextChanged += new System.EventHandler(this.email_TextChanged);
+            // 
+            // password
+            // 
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.password.Font = new System.Drawing.Font("Helvetica", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.password.Location = new System.Drawing.Point(117, 194);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(314, 34);
+            this.password.TabIndex = 9;
+            this.password.Text = "password";
+            this.password.Click += new System.EventHandler(this.password_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(270, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "forgot password??";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.Green;
+            this.button.Font = new System.Drawing.Font("Helvetica", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.button.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button.Location = new System.Drawing.Point(148, 262);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(251, 51);
+            this.button.TabIndex = 11;
+            this.button.Text = "login";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(568, 460);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleShape1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            email.Select();
+        }
+
+        private void email_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void password_Click(object sender, EventArgs e)
+        {
+            password.UseSystemPasswordChar = true;
+            password.Text = "";
+        }
+
+        private void username_Click(object sender, EventArgs e)
+        {
+            email.Text = "";
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            
+            perpustakaan tampil = new perpustakaan();
+            tampil.Show();
+            this.Hide();
+        }
+    }
+}
